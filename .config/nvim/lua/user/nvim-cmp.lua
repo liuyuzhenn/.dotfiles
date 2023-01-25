0c58b2088a56dbe9af80ef68cf4fa4252264f118
liuyuzhen <237925240@qq.com>
2023-01-25 22:27:35 +0800

    update

--- .config/nvim/lua/user/nvim-cmp.lua
+++ .config/nvim/lua/user/nvim-cmp.lua
@@ -20,7 +20,11 @@ cmp.setup({
 		-- documentation = cmp.config.window.bordered(),
 	},
 	formatting = {
-		format = lspkind.cmp_format({ wirth_text = false, maxwidth = 50 }),
+		format = lspkind.cmp_format({
+			mode = 'symbol_text', -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+		})
 	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
