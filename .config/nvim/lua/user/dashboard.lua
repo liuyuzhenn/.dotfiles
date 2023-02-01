a637c407b45942d7607b6a53c97b27ba0b36d9ab
liuyuzhen <237925240@qq.com>
2023-02-01 20:05:00 +0800

    dashboard header

--- .config/nvim/lua/user/dashboard.lua
+++ .config/nvim/lua/user/dashboard.lua
@@ -3,22 +3,24 @@ if not status_ok then
 	return
 end
 
+vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = "#99ff99" })
+-- vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = "#ffffff", bg = "#333333" })
+
 local header = {
 	[[]],
 	[[]],
-	[[        ▄▄▄▄▄███████████████████▄▄▄▄▄     ]],
-	[[      ▄██████████▀▀▀▀▀▀▀▀▀▀██████▀████▄   ]],
-	[[     █▀████████▄             ▀▀████ ▀██▄  ]],
-	[[    █▄▄██████████████████▄▄▄         ▄██▀ ]],
-	[[     ▀█████████████████████████▄    ▄██▀  ]],
-	[[       ▀████▀▀▀▀▀▀▀▀▀▀▀▀█████████▄▄██▀    ]],
-	[[         ▀███▄              ▀██████▀      ]],
-	[[           ▀██████▄        ▄████▀         ]],
-	[[              ▀█████▄▄▄▄▄▄▄███▀           ]],
-	[[                ▀████▀▀▀████▀             ]],
-	[[                  ▀███▄███▀               ]],
-	[[                     ▀█▀                  ]],
-	[[]]
+	[[]],
+	[[]],
+	[[]],
+	[[]],
+	[[███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗]],
+	[[████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║]],
+	[[██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║]],
+	[[██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║]],
+	[[██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║]],
+	[[╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝]],
+	[[]],
+	[[]],
 }
 
 db.setup({
@@ -58,34 +60,6 @@ db.setup({
 	}
 })
 
--- db.custom_center = {
--- 	{ icon = '  ',
--- 		desc = 'Recently latest session                 ',
--- 		shortcut = 'SPC g l',
--- 		action = 'SessionLoad' },
--- 	{ icon = '  ',
--- 		desc = 'Find  File                              ',
--- 		action = 'Telescope find_files find_command=find',
--- 		shortcut = 'SPC f f' },
--- 	{ icon = '  ',
--- 		desc = 'File Browser                            ',
--- 		action = 'Telescope file_browser',
--- 		shortcut = 'SPC f b' },
--- 	{ icon = '  ',
--- 		desc = 'Find  word                              ',
--- 		action = 'Telescope live_grep',
--- 		shortcut = 'SPC f g' },
--- }
---
--- vim.api.nvim_set_keymap('n', '<leader>gl', '<Cmd>SessionLoad<CR>', { noremap = true, silent = false })
--- vim.api.nvim_set_keymap('n', '<leader>gs', '<Cmd>SessionSave<CR>', { noremap = true, silent = false })
--- vim.api.nvim_create_autocmd('User', {
--- 	pattern = 'DBSessionSavePre',
--- 	callback = function()
--- 		pcall(vim.cmd, 'NvimTreeClose')
--- 	end,
--- })
---
 -- vim.cmd([[
 -- hi link DashboardHeader Label
 -- hi link DashboardCenter Identifier
