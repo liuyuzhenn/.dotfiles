837d7fb7ee991235e454338ca9e833b1d6813cdb
liuyuzhen <237925240@qq.com>
2023-08-12 19:06:57 +0800

    update

--- .config/nvim/lua/user/lspconfig.lua
+++ .config/nvim/lua/user/lspconfig.lua
@@ -7,8 +7,6 @@ local util = lspconfig.util
 
 -- key mappings
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
