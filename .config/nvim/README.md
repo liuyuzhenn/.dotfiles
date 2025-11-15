9030f4c6183e8527ca88234faa5d1cabea05c532
liuyuzhen <237925240@qq.com>
2025-11-15 23:46:13 +0800

    nvim as submodule

--- .config/nvim/README.md
+++
@@ -1,6 +0,0 @@
-# Neovim Configurations
-
-Install markdown-preview
-```bash
-cd ~/.local/share/nvim/plugged/markdown-preview.nvim/ && npm install
-``
--- .config/nvim/lua/plugins/nvim-surround.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"git@github.com:kylechui/nvim-surround.git",
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	event = "VeryLazy",
-	config = function()
-		require("nvim-surround").setup({})
-	end
-}
